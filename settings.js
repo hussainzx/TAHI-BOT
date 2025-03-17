3041bf1985f0cc77bb526d9b44d034d26213f4d2
hussainzx <hussainzx5x@gmail.com>
2025-03-17 11:01:26 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "~Your ✘ baby❤️‍🩹😍",
+  botOwner: 'Hᴜssᴀɪɴ 〆ᬊᬁ🇧🇩💝', // Your name
+  ownerNumber: '8801334089349', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
